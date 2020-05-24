b04fcad19c73798838a4ece245ba0a319ada2d5c
Ajay Arora <arora.ajay06@gmail.com>
2020-05-24 14:58:52 +0530

    my portfolio website
    Signed-off-by: Ajay Arora <arora.ajay06@gmail.com>

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_home(username=None, post_id=None):
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_into_database(data):
+	with open('database.txt', mode='a') as database:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		file = database.write(f'{email},{subject},{message}\n')
+
+def write_into_csv(data):
+	with open('database.csv', mode='a') as database2:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		data = request.form.to_dict()
+		write_into_csv(data)
+		return redirect('/thankyou.html')
